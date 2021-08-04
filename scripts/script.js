f8f6af0dafc9ccb0a1280c5fca0b6d44b1c3f9c2
hotShot1211 <anshusuperstar15@gmail.com>
2021-08-04 13:39:12 +0530

    add-del/css-lib-detail-read

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,14 @@
 
 let close = document.querySelector('.close button');
-let form = document.querySelector('.form-cntnr');
+let formCntnr = document.querySelector('.form-cntnr');
+let display = document.querySelector(".book-display")
 let add = document.querySelector('img');
-close.addEventListener('click', () => {
-    form.style.display = 'none';
-    add.style.display = 'block';
-});
-
-add.addEventListener('click', () => {
-    form.style.display = 'block';
-    add.style.display = 'none';
-})
-
 let myLibrary = [];
+let form = document.querySelector("form");
+let val;
+let newBook;
+let read;
+
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -24,6 +20,73 @@ function Book(title, author, pages, read){
     }
 }
 
+function bookDisplay(myLibrary){
+    display.innerHTML = "";
+    let i = 0;
+    myLibrary.forEach(book => {
+        let div = document.createElement("div");
+        div.classList.add("book-1");
+        // div.setAttribute("data-number", i)
+        div.innerHTML = `
+    <div class="delete"><button id="${i}">X</button></div>
+    <div class="details">
+        <div class="title">${book.title}</div>
+        <div class="author">author: ${book.author}</div>
+        <div class="pages">pages: ${book.pages}</div>
+        <div class="read">${book.read}</div>
+
+    </div>`
+        display.append(div);
+        i++;
+
+
+    })
+}
+
+function deleteBook(){
+    let del = document.querySelectorAll(".delete button");
+    del.forEach(delButton => {
+        delButton.addEventListener("click", () =>  {
+            let number = delButton.id;
+            myLibrary.splice(number, 1);
+            bookDisplay(myLibrary);
+        })
+    })
+
+}
+close.addEventListener('click', () => {
+    formCntnr.style.display = 'none';
+    add.style.display = 'block';
+});
+
+add.addEventListener('click', () => {
+    formCntnr.style.display = 'block';
+    add.style.display = 'none';
+})
+
+
+
+form.addEventListener("submit", function (event) {
+    val = []
+    for (let i = 0; i < form.length; i++) {
+        val.push(form.elements[i].value);
+    }
+    console.log(val);
+    read = (val[3] == "on" ? "read" : "not yet");
+    newBook = new Book(val[0], val[1], val[2], read);
+    addBookToLibrary(newBook);
+    bookDisplay(myLibrary);
+    for (let i = 0; i < form.length; i++) {
+        console.log(form.elements[i]);
+        form.elements[i].value = "";
+    }
+    formCntnr.style.display = 'none';
+    add.style.display = 'block';
+    event.preventDefault();
+    display.click();
+
+})
+display.addEventListener('click', deleteBook)
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '256', "Not Yet Read");
 
 let harryPotter = new Book('Harry Potter', 'J.K. Rowling', '250', 'read');
@@ -33,11 +96,13 @@ function addBookToLibrary(newBook){
 
 addBookToLibrary(theHobbit);
 addBookToLibrary(harryPotter);
-console.log(myLibrary[1]);
 
 
-console.log(harryPotter.info());
 
-myLibrary.forEach(book => {
-    console.log(book);
-})
+
+
+
+
+
+
+
--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,12 @@
 let myLibrary = [];
+let display = document.querySelector(".book-display")
 
-function Book(title, author, pages, read){
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function(){
+    this.info = function () {
         return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
     }
 }
@@ -13,7 +14,7 @@ function Book(title, author, pages, read){
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '256', "Not Yet Read");
 
 let harryPotter = new Book('Harry Potter', 'J.K. Rowling', '250', 'read');
-function addBookToLibrary(newBook){
+function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
 }
 
@@ -27,3 +28,45 @@ console.log(harryPotter.info());
 myLibrary.forEach(book => {
     console.log(book);
 })
+
+let form = document.querySelector("form");
+let val;
+console.log(form);
+let newBook;
+let read;
+form.addEventListener("submit", function (event) {
+    val = []
+    display.innerHTML = "";
+    for (let i = 0; i < form.length; i++) {
+        val.push(form.elements[i].value);
+    }
+    console.log(val);
+    read = (val[3] == "on" ? "read" : "not yet");
+    newBook = new Book(val[0], val[1], val[2], read);
+    addBookToLibrary(newBook);
+    let i = 0;
+    myLibrary.forEach(book => {
+        let div = document.createElement("div");
+        div.classList.add("book-1");
+        div.setAttribute("data-number", i)
+        div.innerHTML = `
+    <div class="close"><button>X</button></div>
+    <div class="details">
+        <div class="title">${book.title}</div>
+        <div class="author">author: ${book.author}</div>
+        <div class="pages">pages: ${book.pages}</div>
+        <div class="read">${book.read}</div>
+
+    </div>`
+        display.append(div);
+        i++;
+
+    })
+    for (let i = 0; i < form.length; i++) {
+        form.elements[i].value = "";
+    }
+    event.preventDefault();
+})
+
+
+
